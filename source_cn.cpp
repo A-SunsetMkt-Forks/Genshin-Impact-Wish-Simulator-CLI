343534f7ecdbd301faf0029dd180f9dd58099164
Jirehlov <34600796+Jirehlov@users.noreply.github.com>
2021-05-28 19:52:23 +0800

    Revert "||"
    This reverts commit 8dd3789713bc8e2cbfb7292744188a3a46248d84.

--- source_cn.cpp
+++ source_cn.cpp
@@ -68,7 +68,7 @@
                     type = 2;\
                     five_count_c++;\
                     kind = rspick(nup_five_c, 5);\
-                    if (five_check[0] == 1) five_star_guarantee_number = 0; else five_star_guarantee_number = 1;\
+                    if(five_check[0] == 1) five_star_guarantee_number = 0; else five_star_guarantee_number = 1;\
                 }\
             }
 #define I_4() {star = 4;\
@@ -95,8 +95,7 @@
                             four_count_c++;\
                             unmet4_c = 0;\
                             kind = rspick(nup_four_c, size_nup_four_c);\
-                            for (int temp1189 = 0; temp1189 < 8; temp1189++) { focheck += kind == four_check[temp1189]; }\
-                            if ( focheck )  four_star_guarantee_number = 0; else four_star_guarantee_number = 1;\
+                            if ((kind == four_check[0] || kind == four_check[1] || kind == four_check[2] || kind == four_check[3] || kind == four_check[4] || kind == four_check[5] || kind == four_check[6] || kind == four_check[7])) four_star_guarantee_number = 0; else four_star_guarantee_number = 1;\
                         }\
                         else {\
                             type = 3;\
@@ -112,8 +111,7 @@
                             four_count_c++;\
                             unmet4_c = 0;\
                             kind = rspick(nup_four_c, size_nup_four_c);\
-                            for (int temp1189 = 0; temp1189 < 8; temp1189++) { focheck += kind == four_check[temp1189]; }\
-                            if ( focheck )  four_star_guarantee_number = 0; else four_star_guarantee_number = 1;\
+                            if ((kind == four_check[0] || kind == four_check[1] || kind == four_check[2] || kind == four_check[3] || kind == four_check[4] || kind == four_check[5] || kind == four_check[6] || kind == four_check[7])) four_star_guarantee_number = 0; else four_star_guarantee_number = 1;\
                         }\
                         else {\
                             type = 3;\
@@ -136,8 +134,7 @@
                             four_count_c++;\
                             unmet4_c = 0;\
                             kind = rspick(nup_four_c, size_nup_four_c);\
-                            for (int temp1189 = 0; temp1189 < 8; temp1189++) { focheck += kind == four_check[temp1189]; }\
-                            if ( focheck )  four_star_guarantee_number = 0; else four_star_guarantee_number = 1;\
+                            if ((kind == four_check[0] || kind == four_check[1] || kind == four_check[2] || kind == four_check[3] || kind == four_check[4] || kind == four_check[5] || kind == four_check[6] || kind == four_check[7])) four_star_guarantee_number = 0; else four_star_guarantee_number = 1;\
                         }\
                     }\
                 }\
@@ -164,8 +161,7 @@
                         type = 2;\
                         five_count_w++;\
                         kind = rspick(nup_five_w, 10);\
-                        for (int temp1189 = 0; temp1189 < 8; temp1189++) { ficheck += kind == five_check[temp1189]; }\
-                        if ( ficheck ) five_star_guarantee_number = 0; else five_star_guarantee_number = 1;\
+                        if((kind == five_check[0] || kind == five_check[1] || kind == five_check[2] || kind == five_check[3] || kind == five_check[4] || kind == five_check[5] || kind == five_check[6] || kind == five_check[7])) five_star_guarantee_number = 0; else five_star_guarantee_number = 1;\
                     }\
                 }
 #define II_4() {star = 4;\
@@ -199,8 +195,7 @@
                                 four_count_w++;\
                                 unmet4_w = 0;\
                                 kind = rspick(nup_four_w, size_nup_four_w);\
-                                for (int temp1189 = 0; temp1189 < 8; temp1189++) { focheck += kind == four_check[temp1189]; }\
-                            if ( focheck )  four_star_guarantee_number = 0; else four_star_guarantee_number = 1;\
+                                if ((kind == four_check[0] || kind == four_check[1] || kind == four_check[2] || kind == four_check[3] || kind == four_check[4] || kind == four_check[5] || kind == four_check[6] || kind == four_check[7])) four_star_guarantee_number = 0; else four_star_guarantee_number = 1;\
                             }\
                         }\
                         else if (unmet4_w > 14) {\
@@ -216,8 +211,7 @@
                                 four_count_w++;\
                                 unmet4_w = 0;\
                                 kind = rspick(nup_four_w, size_nup_four_w);\
-                                for (int temp1189 = 0; temp1189 < 8; temp1189++) { focheck += kind == four_check[temp1189]; }\
-                            if ( focheck )  four_star_guarantee_number = 0; else four_star_guarantee_number = 1;\
+                                if ((kind == four_check[0] || kind == four_check[1] || kind == four_check[2] || kind == four_check[3] || kind == four_check[4] || kind == four_check[5] || kind == four_check[6] || kind == four_check[7])) four_star_guarantee_number = 0; else four_star_guarantee_number = 1;\
                             }\
                         }\
                         else {\
@@ -226,8 +220,7 @@
                                 four_count_w++;\
                                 unmet4_w = 0;\
                                 kind = rspick(nup_four_w, size_nup_four_w);\
-                                for (int temp1189 = 0; temp1189 < 8; temp1189++) { focheck += kind == four_check[temp1189]; }\
-                            if ( focheck )  four_star_guarantee_number = 0; else four_star_guarantee_number = 1;\
+                                if ((kind == four_check[0] || kind == four_check[1] || kind == four_check[2] || kind == four_check[3] || kind == four_check[4] || kind == four_check[5] || kind == four_check[6] || kind == four_check[7])) four_star_guarantee_number = 0; else four_star_guarantee_number = 1;\
                             }\
                             else {\
                                 type = 2;\
@@ -522,10 +515,8 @@ int main() {
         size_nup_four_c = 1,
         size_nup_four_w = 1,
         three_g[18] = { 0 },
-        five_check[8] = { 127, 127, 127, 127, 127, 127, 127, 127 },
-        four_check[8] = { 127, 127, 127, 127, 127, 127, 127, 127 },
-        ficheck = 0,
-        focheck = 0;
+        five_check[8] = { 127 },
+        four_check[8] = { 127 };
         long long int countx = 0,
         five_count = 0,
         five_count_c = 0,
