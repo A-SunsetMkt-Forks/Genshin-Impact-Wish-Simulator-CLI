87731af782a1a12f05330267b215cf009e174e61
Jirehlov <34600796+Jirehlov@users.noreply.github.com>
2021-08-08 14:05:29 +0800

    v2.0.10

--- en.h
+++ en.h
@@ -1,5 +1,5 @@
 ﻿#ifndef EN_H
-#define S_1 "WELCOME TO GENSHIN IMPACT WISH SIMULATOR CLI V2.0.9"
+#define S_1 "WELCOME TO GENSHIN IMPACT WISH SIMULATOR CLI V2.0.10"
 #define S_2 "by Jirehlov Research"
 #define S_3 "Source code available on GitHub: Jirehlov/Genshin-Impact-Wish-Simulator-CLI"
 #define S_4 "MIT LICENSE"
